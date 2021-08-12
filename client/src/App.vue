090dced2bbb1557f0abe23393077a0d2b4828771
JN <gooday6983@gmail.com>
2021-08-13 02:12:02 +0900

    Updates on login

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,11 +11,36 @@
 
 <script>
 import TopBar from './components/TopBar.vue';
-
+import { getCookie } from '@/jslib/cookieIO.js';
+import axios from 'axios';
 
 export default {
 	components: {
 		TopBar
+	},
+
+	beforeMount() {
+		var seToken=getCookie('sessionToken');
+		if(seToken) {
+			axios.get('/isOnline_process', {
+				header: {
+					'Authorization': seToken
+				}
+			})
+			.then((res)=> {
+				if(res.status===200) {
+					console.log(res.data);
+					this.$store.dispatch('setUser',{
+						isLogin: true,
+						isAdmin: res.data.authority_level>1,
+						name: res.data.name
+					})
+				}
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+		}
 	}
 }
 </script>
@@ -23,6 +48,7 @@ export default {
 <style>
 html, body {
 	width: 100vw;
+	min-height: 100vh;
 	margin: 0;
 }
 
