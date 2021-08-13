ac0dc624cd723220e46c2b93900b89cd2a1bc8a2
Jinu <gooday6983@gmail.com>
2021-08-13 16:52:12 +0900

    Add searchbox

---
+++ client/src/components/Top/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="searchbox">
+    <form @submit.prevent="handleSearch">
+      <input type="text" placeholder="Search..." name="search"/>
+      <input type="submit"/>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSearch: function(event) {
+      this.$router.push('/to/'+event.target.search.value);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,16 +1,19 @@
 <template>
 	<nav class="top">
-		<span>LOGO</span>
+		<router-link to="/"><img src="./Top/LOGO.png" id="header-logo"/></router-link>
+		<SearchBox/>
 		<MenuBox/>
 	</nav>
 </template>
 
 <script>
 import MenuBox from './Top/MenuBox.vue';
+import SearchBox from './Top/SearchBox';
 
 export default {
 	components: {
-		MenuBox
+		MenuBox,
+		SearchBox
 	}
 };
 </script>
@@ -22,5 +25,25 @@ nav.top {
 	justify-content: space-between;
 	align-items: center;
 	background: linear-gradient(0.375turn, #fff 20%, #ffc3c3 65%, #c3c3ff 95%);
+}
+
+@media screen and (max-width: 770px) {
+	nav.top {
+		height: 70px;
+	}
+	#header-logo {
+		width: 162px;
+		height: 60px;
+	}
+}
+
+@media screen and (min-width: 770px) {
+	nav.top {
+		height: 120px;
+	}
+	#header-logo {
+		width: 270px;
+		height: 100px;
+	}
 }
 </style>
