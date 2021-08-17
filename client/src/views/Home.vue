a1f16b09e90df61298e63e924ecc0af240fd5870
KimDajeong <cgscwg@gmail.com>
2021-08-18 01:25:58 +0900

    Home.vue 페이지 스타일링

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="full">
-      <img class="image" src="./images/korea-main.jpg" alt="">
+      <img class="image" src="./images/mainImage.jpg" alt="">
       <div class="homeMain">
           <div div class="recommendBtn"
           v-for="item in this.$store.state.places"
@@ -25,6 +25,13 @@ export default {
 </script>
 
 <style>
+@font-face {
+    font-family: 'Recipekorea';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/Recipekorea.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
 .full {
     position: relative;
 }
@@ -34,9 +41,11 @@ width: 100%;
 }
 
 .homeMain {
+    text-align: right;
     width: 100%;
     position: absolute;
-    bottom: 1%;
+    top: 3%;
+    right: 3%;
 }
 
 .recommendBtn {
@@ -44,27 +53,33 @@ width: 100%;
 }
 
 .recommendBtn button {
-    margin: 1%;
-    border-radius: 10px;
-    border: 1px solid black;
-    background-color: rgba(255, 355, 355, 0.3);
+    /* margin: 1%; */
+    /* border-radius: 10px; */
+    border: 1px solid rgba(0, 0, 0, 0);
+    border-bottom: 1px solid black;
+    background-color: rgba(0, 0, 0, 0);
+    font-family: 'Recipekorea';
+    /* text-decoration: underline; */
+    /* transform: scale(1.4); */
+    /* text-underline-position: under; */
+    text-transform: uppercase;
 }
 
 @media screen and (max-width: 770px) {
 	.recommendBtn button {
-    font-size: 1.5rem;
+    font-size: 1.1rem;
     font-weight: 600;
     text-shadow: -0.5px 0 #D3D3D3, 0 0.5px #D3D3D3, 0.5px 0 #D3D3D3, 0 -0.5px #D3D3D3;
-    padding: 5px 15;
+    padding: 1px 10px;
 }
 }
 
 @media screen and (min-width: 770px) {
 	.recommendBtn button {
-    font-size: 2rem;
+    font-size: 1.8rem;
     font-weight: 600;
     text-shadow: -1px 0 #D3D3D3, 0 1px #D3D3D3, 1px 0 #D3D3D3, 0 -1px #D3D3D3;
-    padding: 5px 30px;
+    padding: 1px 10px;
 }
 }
 </style>
