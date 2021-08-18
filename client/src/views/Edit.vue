40fc12b0da4d7b0cc6d1a1dd28d0232926166658
JN <gooday6983@gmail.com>
2021-08-18 15:44:59 +0900

    Add /edit page

---
+++ client/src/views/Edit.vue
@@ -0,0 +1,81 @@
+<template>
+	<article class="editor">
+		<form @submit.prevent="handleUpload">
+			<div class="inputBox">
+				<label for="name">Enter place name: </label>
+				<input type="text" name="name"/>
+			</div>
+			<div class="inputBox">
+				<label for="description">Enter place description: </label>
+				<input type="text" name="description"/>
+			</div>
+			<div class="inputBox">
+				<label for="image">Put image file: </label>
+				<input type="file" name="image" accept="image/png, image/jpeg"/>
+			</div>
+			<div class="inputBox">
+				<label for="imageCopyright">Enter image copyright: </label>
+				<input type="text" name="imageCopyright"/>
+			</div>
+			<div class="inputBox">
+				<input type="submit"/>
+			</div>
+		</form>
+	</article>
+</template>
+
+<script>
+import axios from 'axios';
+import { getCookie } from '@/jslib/cookieIO';
+
+export default {
+	methods: {
+		handleUpload: function(event) {
+			const frm = new FormData();
+			if(event.target.name.value) {
+				frm.append('name', event.target.name.value);
+				if(event.target.description.value) {
+					frm.append('description', event.target.description.value);
+				}
+				if(event.target.image.files[0]) {
+					frm.append('image', event.target.image.files[0]);
+					if(event.target.imageCopyright.value) {
+						frm.append('imageCopyright', event.target.imageCopyright.value);
+					}
+				}
+				axios.get("/placeinfo/"+event.target.name.value)
+				.then(()=> {
+					axios.patch("/placeinfo/"+event.target.name.value,frm, {
+						headers: {
+							"Authorization": "Bearer "+getCookie("accessToken")
+						}
+					})
+					.then(()=> {
+						window.alert('Uploaded!');
+					})
+					.catch((err)=> {
+						window.alert('Upload failed!');
+						console.error(err);
+					});
+				})
+				.catch(()=> {
+					axios.post("/placeinfo",frm, {
+						headers: {
+							"Authorization": "Bearer "+getCookie("accessToken")
+						}
+					})
+					.then(()=> {
+						window.alert("Uploaded!");
+					})
+					.catch((err)=> {
+						window.alert('Upload failed!');
+						console.error(err);
+					});
+				});
+			} else {
+				window.alert('Enter place name');
+			}
+		}
+	}
+}
+</script>
